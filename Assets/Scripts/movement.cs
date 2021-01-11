4aa39abe419df9e8cab96f41f9414f5bf7a981f1
Boussin Pierre <boussinpierre@gmail.com>
2021-01-11 11:40:13 +0100

    Add movement + fire

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -7,7 +7,12 @@ public class movement : MonoBehaviour
 {
 
     public InputAction zqsd;
+    public InputAction fireAction;
     public CharacterController controller;
+    public InputAction mouseshoot;
+
+    public GameObject projectile;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -15,21 +20,36 @@ public class movement : MonoBehaviour
     void OnEnable()
     {
         zqsd.Enable();
+        fireAction.Enable();
+        mouseshoot.Enable();
     }
     void OnDisable()
     {
         zqsd.Disable();
+        fireAction.Disable();
+        mouseshoot.Disable();
     }
     // Update is called once per frame
     void Update()
     {
+
         Vector2 inputVector = zqsd.ReadValue<Vector2>();
         Vector3 finalVector = new Vector3();
 
         finalVector.x = inputVector.x;
         finalVector.z = inputVector.y;
         controller.Move(finalVector * Time.deltaTime * 5f);
+
+        if (fireAction.triggered)
+        {
+          GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 600);
+        }
+       
+
     }
+
+    
 
    
 }
